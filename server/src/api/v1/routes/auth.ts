46abc662899937555186778686fd2829c3f1b7e0
William Inyam <sw.inyam@outlook.com>
2025-08-11 12:58:05 +0000

    :star: feat(auth): add validation to register endpoint

--- server/src/api/v1/routes/auth.ts
+++ server/src/api/v1/routes/auth.ts
@@ -1,10 +1,39 @@
 import express, { type Request, type Response } from "express";
 import AuthController from "../controllers/AuthController.js";
+import { body } from "express-validator";
 
 const authController = new AuthController();
 const authRouter = express.Router();
 
-authRouter.post("/login", authController.login);
+authRouter.post(["/login", "/signin"], authController.login);
+authRouter.post(
+  ["/", "/signup", "/register"],
+  [
+    body().notEmpty().withMessage("data required"),
+    body("email")
+      .notEmpty()
+      .withMessage("required")
+      .isEmail()
+      .withMessage("invalid email"),
+    body("role")
+      .notEmpty()
+      .withMessage("required")
+      .isIn(["admin", "user"])
+      .withMessage("invalid user role"),
+    body("termsAccepted")
+      .notEmpty()
+      .withMessage("required")
+      .isBoolean({ strict: true })
+      .withMessage("invalid boolean"),
+    body(["password"])
+      .notEmpty()
+      .withMessage("required")
+      .isString()
+      .withMessage("invalid string"),
+  ],
+
+  authController.create
+);
 // handle 404
 authRouter.use("/*", (_: Request, res: Response): void => {
   res.status(404).json({
