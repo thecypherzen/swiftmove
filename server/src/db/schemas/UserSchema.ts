5670f5e58a8b1cde87de621d775e0e775d233124
William Inyam <sw.inyam@outlook.com>
2025-08-11 14:05:27 +0000

    :star: feat(db): create user schema

--- server/src/db/schemas/UserSchema.ts
+++ server/src/db/schemas/UserSchema.ts
@@ -1,5 +1,55 @@
 import mongoose from "mongoose";
+const { v4: uuidv4 } = require("uuid");
 
-const UserSchema = new mongoose.Schema({ name: String });
+const UserSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensures no two users can have the same email
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  acceptTerms: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+// Virtual for id
+UserSchema.virtual("id").get(function () {
+  return this._id;
+});
+
+// Ensure virtuals are included in toJSON and toObject
+UserSchema.set("toJSON", {
+  virtuals: true,
+  transform: (_, ret) => {
+    const { _id, __v, ...rest } = ret;
+    return { ...rest, id: _id };
+  },
+});
+
+UserSchema.set("toObject", {
+  virtuals: true,
+  transform: (_, ret) => {
+    const { _id, __v, ...rest } = ret;
+    return { ...rest, id: _id };
+  },
+});
 
 export { UserSchema };
