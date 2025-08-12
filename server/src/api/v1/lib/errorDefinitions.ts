f3b9754a4411e14a52e5eb84edaccdc23ba61e6f
William Inyam <sw.inyam@outlook.com>
2025-08-12 23:21:03 +0000

    :up: update(lib): improve err code map + add err cause generator to err class

--- server/src/api/v1/lib/errorDefinitions.ts
+++ server/src/api/v1/lib/errorDefinitions.ts
@@ -1,16 +1,19 @@
 import { stringify } from "querystring";
+import { isArray } from "util";
 
 export const errors: ServerErrorsType = {
   auth: {
     default: {
       errno: "20",
       desc: "Unauthorised",
+      meaning: "Authentication not passed",
       statusCode: 401,
     },
     "20": {
       errno: "20",
       desc: "",
       statusCode: 401,
+      meaning: "Authentication not passed",
     },
   },
   validation: {
@@ -18,43 +21,51 @@ export const errors: ServerErrorsType = {
       errno: "31",
       desc: "Invalid Data",
       statusCode: 400,
+      meaning: "Data sent not in expected format",
     },
     "31": {
       errno: "31",
       desc: "Invalid Data",
       statusCode: 400,
+      meaning: "Data sent not in expected format",
     },
     "32": {
       errno: "32",
       desc: "No data sent",
       statusCode: 400,
+      meaning: "Data expected but none sent in request",
     },
   },
   server: {
     default: {
-      errno: "51",
+      errno: "50",
       desc: "Internal Server Error",
       statusCode: 500,
+      meaning: "Generic, random or uncovered server error",
     },
-    "51": {
-      errno: "51",
+    "50": {
+      errno: "50",
       desc: "Internal Server Error",
       statusCode: 500,
+      meaning: "Generic, random or uncovered server error",
     },
     "52": {
       errno: "52",
       desc: "Internal Server Error",
       statusCode: 500,
+      meaning: "Database client connection related error",
     },
     "53": {
       errno: "53",
       desc: "Internal Server Error",
       statusCode: 500,
+      meaning: "Database operation failed",
     },
     "54": {
       errno: "53",
       desc: "Inernal Server Error",
       statusCode: 500,
+      meaning: "Database query failed",
     },
   },
   success: {
@@ -62,28 +73,58 @@ export const errors: ServerErrorsType = {
       errno: "0",
       desc: "Successful",
       statusCode: 200,
+      meaning: "",
     },
     "0": {
       errno: "0",
       desc: "Successful",
       statusCode: 200,
+      meaning: "Operation successfully executed",
     },
     "01": {
       errno: "01",
       desc: "Created",
       statusCode: 201,
+      meaning: "New resource creation was successful",
     },
   },
 };
 
 export class ServerError extends Error {
-  errno?: string = "51";
+  errno: string = "50";
   #name: string;
 
-  constructor({ message, errno = "51", cause }: ServerErrorPropsType) {
+  constructor({ message, errno = "50", cause }: ServerErrorPropsType) {
     super(message, { cause });
     this.errno = errno;
     this.#name = "ServerError";
+  }
+  /**
+   * Creates a server error cause from an error object.
+   * Expects to be called only when the object is a native or
+   * derrived type of the Error object.
+   * @param {Object} err Some native or derrived Error object
+   * @returns {ServerErrorCauseType} The cause of the error
+   * @throws {ServerError} - If type of object passed is not a native or
+   * derrived Error object.
+   */
+  static constructErrorCause<T extends ServerError>(
+    err: T
+  ): ServerErrorCauseType {
+    if (!(err instanceof Error)) {
+      throw new ServerError({
+        message: `Native or derrived Error expected but got ${typeof err}`,
+      });
+    }
+    const c: Record<string, any> = {
+      name: err?.name ?? "Error",
+      message: err.message,
+      stack: err.stack,
+    };
+    if (err.errno) {
+      c.errno = err.errno;
+    }
+    return c as ServerErrorCauseType;
   }
 
   get name() {
@@ -132,6 +173,7 @@ export type ErrorNumberType = {
   errno: string;
   desc: string;
   statusCode: number;
+  meaning: string;
 };
 export type ServerErrorsType = {
   [key in ServerErrorCodeType]: {
