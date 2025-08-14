8c22214433855c735a44bedefab7887fcf09d5eb
William Inyam <sw.inyam@outlook.com>
2025-08-14 00:35:20 +0000

    :truck::pencil: update(lib): modify passwords library name + add docstrings

--- server/src/api/v1/lib/PasswordLibrary.ts
+++ server/src/api/v1/lib/PasswordLibrary.ts
@@ -1,7 +1,24 @@
 import { argon2id, hash, verify } from "argon2";
 import { ServerError } from "./ServerError.js";
 
+/**
+ * The Passwords Library
+ *
+ * Contains definitions for passwords hashing and verification
+ * functions. Currently supports only argon2 but can be extended
+ * to support various algorithms.
+ *
+ * Exposed as a default export
+ */
+
 export default {
+  /**
+   * Hashes a password using argon2id algorithm
+   * @function hash
+   * @param {string} password - the original password to hash
+   * @returns {Promise<PasswordsResType>} - a promise that resolves into
+   * standardised libary response object.
+   */
   hash: async (password: string): Promise<PasswordsResType> => {
     try {
       const passwordHash = await hash(password, {
@@ -15,6 +32,14 @@ export default {
       return { success: false, result: err };
     }
   },
+  /**
+   * Verifies a password string against an argon2id password string
+   * @function verify
+   * @param {string} hash - the argon2id hash
+   * @param {string} password - the password string
+   * @returns {Promise<PasswordsResType>} - a promise that resolves into
+   * standardised libary response object.
+   */
   verify: async (hash: string, password: string): Promise<PasswordsResType> => {
     try {
       const matches = await verify(hash, password);
