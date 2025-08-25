fe76d618013991adaa14e22fde53592761488334
William Inyam <sw.inyam@outlook.com>
2025-08-25 05:18:49 +0100

    :up: update(layout): add breadcrumb to AppLayout

--- client/src/components/layouts/AppLayout.tsx
+++ client/src/components/layouts/AppLayout.tsx
@@ -4,11 +4,17 @@ import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 import AppSidebar from "../sidebars/AppSidebar";
 import { UseIsMobile } from "@/hooks/UseIsMobile";
 import { UseBreadcrumbs } from "@/hooks/Breadcrumbs";
+import { useEffect } from "react";
 
 const AppLayout = () => {
   const isMobile = UseIsMobile();
-  const { breadcrumb: Breadcrumbs } = UseBreadcrumbs();
+  const { breadcrumb: Breadcrumbs, ref: crumbsRef } = UseBreadcrumbs();
 
+  useEffect(() => {
+    console.log("current crumbs:", crumbsRef.current, crumbsRef);
+  }, [crumbsRef.current]);
+
+  //const scrolled = UseScroll(50, crumbsRef);
   return (
     <main className="min-h-screen bg-background dark:bg-dark">
       <SidebarProvider offsetT={true}>
@@ -18,7 +24,7 @@ const AppLayout = () => {
         <main className="w-full">
           <div className="flex items-center gap-1 py-2 w-full">
             {!isMobile && <SidebarTrigger className="mx-4" />}
-            {Breadcrumbs}
+            {<Breadcrumbs />}
           </div>
           <Outlet />
         </main>
