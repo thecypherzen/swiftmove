2e6df2a15bcffe6767dcc14f581336d6017c38a3
William Inyam <sw.inyam@outlook.com>
2025-08-26 23:43:41 +0100

    :up: create(lib): add random image url getter func

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { randomImages } from "@/mock_data/RandomImages";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getRandomImageUrl = () => {
+  const index = Math.floor(Math.random() * 100) % randomImages.length;
+  return randomImages[index];
+};
