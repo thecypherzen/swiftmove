28dad96c7800f32390aeb7b1601531b0e3326b84
William Inyam <sw.inyam@outlook.com>
2025-08-27 05:58:13 +0100

    :truck: update(dashboard): simplify rshipments section file structure

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,13 +1,19 @@
+import RecentShipmentsCard from "@/components/cards/RecentShipmentsCard";
 import DashSummarySection from "@/components/sections/DashSummarySection";
-import RecentShipmentsSection from "@/components/sections/RecentShipmentsSection";
 import PageRouteWrapper from "@/components/wrappers/PageRouteWrapper";
+import { recentShipments } from "@/mock_data/DashboardData";
 
 const UserDashboard = () => {
   return (
     <PageRouteWrapper>
       <div className="route-page grid grid-cols-1 gap-10">
         <DashSummarySection />
-        <RecentShipmentsSection />
+        <div className="w-full grid grid-cols-1 @3xl/page:grid-cols-[2fr_1fr] gap-6">
+          <RecentShipmentsCard
+            title={"Recent Shipments"}
+            data={recentShipments}
+          />
+        </div>
       </div>
     </PageRouteWrapper>
   );
