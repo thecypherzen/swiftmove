b6d9a30bd43adf4dd40c871254c3c79a8c0ac61a
William Inyam <sw.inyam@outlook.com>
2025-08-29 12:08:49 +0100

    :shirt: update: remove debug logs + improve index route page

--- client/src/hooks/UseIsMobile.ts
+++ client/src/hooks/UseIsMobile.ts
@@ -2,12 +2,17 @@ import * as React from "react";
 
 const MOBILE_BREAKPOINT = 768;
 
+/**
+ * A hook that checks if the viewport is of a particular dimension
+ * @function UseIsMobile
+ * @param { number } breakpoint - viewport width to check for
+ *   - defaults to **768**
+ * @returns boolean
+ */
 export function UseIsMobile(breakpoint: number = MOBILE_BREAKPOINT) {
   const [isMobile, setIsMobile] = React.useState<boolean>(false);
 
-  React.useEffect(() => {
-    console.log(`isViewport >= ${breakpoint}: ${isMobile}`);
-  }, [isMobile]);
+  React.useEffect(() => {}, [isMobile]);
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${breakpoint - 1}px)`);
--- client/src/pages/HomePageIndex.tsx
+++ client/src/pages/HomePageIndex.tsx
@@ -3,7 +3,7 @@ import PageRouteWrapper from "@/components/wrappers/PageRouteWrapper";
 const HomePageIndex = () => {
   return (
     <PageRouteWrapper>
-      <div className="route-page w-full min-h-[calc(100%-76px)] flex flex-col items-center justify-center">
+      <div className="w-full h-[calc(100%-80px)] flex flex-col items-center justify-center text-foreground/50 dark:text-foreground/90">
         <h2>Welcome</h2>
         <p>Get started with monitoring your company</p>
       </div>
